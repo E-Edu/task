a78aa273c0c64362b8dea2e874b0d89334e62482
BuildTools <unconfigured@null.spigotmc.org>
2020-03-20 20:47:06 +0100

    Created Spring Project

---
+++ src/test/java/de/themorpheus/edu/taskservice/TaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.themorpheus.edu.taskservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
