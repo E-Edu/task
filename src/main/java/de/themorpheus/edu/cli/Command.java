cb3ef2b4c6204680008e7e611f855facf8cd1c0f
Julian <44580956+JuIez@users.noreply.github.com>
2020-03-21 15:14:18 +0100

    Fixed Request changes (#13)

--- src/main/java/de/themorpheus/edu/cli/Command.java
+++ src/main/java/de/themorpheus/edu/cli/Command.java
@@ -5,17 +5,18 @@ import com.beust.jcommander.JCommander;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
-@Data @RequiredArgsConstructor
+@Data
+@RequiredArgsConstructor
 public class Command<T> {
 
-    private final String name;
-    private final String description;
-    private final String[] alias;
-    private T args;
-    private final CommandExecuter<JCommander, Options, T> executer;
+	private final String name;
+	private final String description;
+	private final String[] alias;
+	private T args;
+	private final CommandExecuter<JCommander, Options, T> executer;
 
-    public Command(String name, String description, CommandExecuter<JCommander, Options, T> executer, Class<T> args, String... alias) throws InstantiationException, IllegalAccessException {
-        this(description, description, alias, executer);
-        this.args = args.newInstance();
-    }
+	public Command(String name, String description, CommandExecuter<JCommander, Options, T> executer, Class<T> args, String... alias) throws InstantiationException, IllegalAccessException {
+		this(description, description, alias, executer);
+		this.args = args.newInstance();
+	}
 }
--- src/main/java/de/themorpheus/edu/cli/CommandExecuter.java
+++ src/main/java/de/themorpheus/edu/cli/CommandExecuter.java
@@ -2,6 +2,6 @@ package de.themorpheus.edu.cli;
 
 @FunctionalInterface
 public interface CommandExecuter<F, S, T> {
-    void execute(F f, S s, T t);
+	void execute(F f, S s, T t);
 }
 
