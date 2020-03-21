a562264a39a8ac565f4c6af7e66824fbf6f5ab7e
Steve Hb <stve.hb@gmail.com>
2020-03-21 18:07:46 +0100

    Use JpaRepository instead of CrudRepository (#40)

--- src/main/java/de/themorpheus/edu/taskservice/database/repository/TaskRepository.java
+++ src/main/java/de/themorpheus/edu/taskservice/database/repository/TaskRepository.java
@@ -1,11 +1,11 @@
 package de.themorpheus.edu.taskservice.database.repository;
 
 import de.themorpheus.edu.taskservice.database.model.TaskModel;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends CrudRepository<TaskModel, Integer> {
+public interface TaskRepository extends JpaRepository<TaskModel, Integer> {
 
 	TaskModel getTaskByTaskId(int taskId);
 
