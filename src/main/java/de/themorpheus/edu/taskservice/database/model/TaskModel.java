fcd930766c57080d1d95201ca3297f20f4525743
PAException <huerkamp.paul@gmail.com>
2020-03-21 22:01:09 +0100

    Create Models (#69) and changed Table names (#68)

--- src/main/java/de/themorpheus/edu/taskservice/database/model/TaskModel.java
+++ src/main/java/de/themorpheus/edu/taskservice/database/model/TaskModel.java
@@ -1,20 +1,27 @@
 package de.themorpheus.edu.taskservice.database.model;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.UUID;
 
+@Data
 @Entity
-@Table(name = "task")
+@Table(name = "task_task")
 public class TaskModel {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private int taskId;
 
+	private UUID authorId;
 	private String task;
+	private int necessaryPoints;
+	private boolean verified;
 
 	public TaskModel(String task) {
 		this.task = task;
