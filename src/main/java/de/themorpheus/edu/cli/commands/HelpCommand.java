795863c5b636cc1a537bc038e10d4785777b1122
Philipp Klein <phklein@uni-bremen.de>
2020-03-21 12:17:56 +0100

    issue/2 removed debugmsg from HelpCommand

--- src/main/java/de/themorpheus/edu/cli/commands/HelpCommand.java
+++ src/main/java/de/themorpheus/edu/cli/commands/HelpCommand.java
@@ -6,7 +6,6 @@ import com.beust.jcommander.Parameters;
 @Parameters(commandDescription = "Shows the help/usage page")
 public class HelpCommand {
 	public static void execute(JCommander commander, HelpCommand cmd) {
-		System.out.println("Test");
 		commander.usage();
 	}
 }
