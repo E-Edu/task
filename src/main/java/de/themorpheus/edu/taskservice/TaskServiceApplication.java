fd3b2b1c37f502328258d7ea25cb12ade8bf7c3c
Steve Hb <stve.hb@gmail.com>
2020-03-21 18:11:18 +0100

    Enabled JpaRepository (#40)

--- src/main/java/de/themorpheus/edu/taskservice/TaskServiceApplication.java
+++ src/main/java/de/themorpheus/edu/taskservice/TaskServiceApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import de.themorpheus.edu.taskservice.cli.CLIManager;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 @SpringBootApplication(scanBasePackages = "de.themorpheus.edu.taskservice")
 public class TaskServiceApplication {
 
