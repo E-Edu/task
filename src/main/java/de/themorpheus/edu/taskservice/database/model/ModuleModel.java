9ae033a34d8389f98491f86b0144f3121623ff8d
PAException <huerkamp.paul@gmail.com>
2020-03-21 18:16:15 +0100

    Added Getters + Setters (#34)

--- src/main/java/de/themorpheus/edu/taskservice/database/model/ModuleModel.java
+++ src/main/java/de/themorpheus/edu/taskservice/database/model/ModuleModel.java
@@ -13,4 +13,22 @@ public class ModuleModel {
 
 	private String displayName;
 
+	public ModuleModel() {
+	}
+
+	public int getModuleId() {
+		return moduleId;
+	}
+
+	public void setModuleId(int moduleId) {
+		this.moduleId = moduleId;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
 }
