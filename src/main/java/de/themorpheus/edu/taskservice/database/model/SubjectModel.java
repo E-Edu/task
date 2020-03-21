512d1bac095217d0b705b9ec99960fd993539718
PAException <huerkamp.paul@gmail.com>
2020-03-21 18:15:06 +0100

    Added Getters + Setters (#32)

--- src/main/java/de/themorpheus/edu/taskservice/database/model/SubjectModel.java
+++ src/main/java/de/themorpheus/edu/taskservice/database/model/SubjectModel.java
@@ -13,4 +13,22 @@ public class SubjectModel {
 
 	private String displayName;
 
+	public SubjectModel() {
+	}
+
+	public int getSubjectId() {
+		return subjectId;
+	}
+
+	public void setSubjectId(int subjectId) {
+		this.subjectId = subjectId;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
 }
