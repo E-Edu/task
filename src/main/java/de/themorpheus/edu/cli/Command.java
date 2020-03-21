ede45509f93aa93edaeffd1fdbdb2acf55bff7b9
PhilippKatze <36547866+PhilippKatze@users.noreply.github.com>
2020-03-21 13:54:50 +0100

    Issue/2 Cli Basic Setup (#28)
    * Created Spring Project
    
    * Delteted unnecessary files
    
    * Changed pom.xml
    
    * issue/2 CLI With Help Command
    
    * issue/2 removed debugmsg from HelpCommand
    
    * issue/2 add options to command
    
    * issue/2 pullrequest changes
    
    * issue/2 pullrequest changes

---
+++ src/main/java/de/themorpheus/edu/cli/Command.java
@@ -0,0 +1,21 @@
+package de.themorpheus.edu.cli;
+
+import com.beust.jcommander.JCommander;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data @RequiredArgsConstructor
+public class Command<T> {
+	
+	private final String name;
+	private final String description;
+	private final String[] alias;
+	private T args;
+	private final CommandExecuter<JCommander, Options, T> executer;
+	
+	public Command(String name, String description, CommandExecuter<JCommander, Options,T> executer,  Class<T> args, String... alias) throws InstantiationException, IllegalAccessException {
+		this(description, description, alias, executer);
+		this.args = args.newInstance();
+	}
+}
