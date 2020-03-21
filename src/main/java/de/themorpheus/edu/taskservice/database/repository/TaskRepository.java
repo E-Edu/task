8fea857187911d1b9a72435f89d0073225bdb4d2
Steve Hb <stve.hb@gmail.com>
2020-03-21 17:52:54 +0100

    Added repository annotation to TaskRepository (#40)

--- src/main/java/de/themorpheus/edu/taskservice/database/repository/TaskRepository.java
+++ src/main/java/de/themorpheus/edu/taskservice/database/repository/TaskRepository.java
@@ -2,7 +2,9 @@ package de.themorpheus.edu.taskservice.database.repository;
 
 import de.themorpheus.edu.taskservice.database.model.TaskModel;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends CrudRepository<TaskModel, Integer> {
 
 	TaskModel getTaskByTaskId(int taskId);
