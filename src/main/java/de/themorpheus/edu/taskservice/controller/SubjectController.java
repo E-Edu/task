2989e9edbd574e85b4a8180db8c23678b4bb9ee3
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-03-22 17:00:57 +0100

    Created method to delete a subject by his display name

--- src/main/java/de/themorpheus/edu/taskservice/controller/SubjectController.java
+++ src/main/java/de/themorpheus/edu/taskservice/controller/SubjectController.java
@@ -4,6 +4,8 @@ import de.themorpheus.edu.taskservice.database.model.SubjectModel;
 import de.themorpheus.edu.taskservice.database.repository.SubjectRepository;
 import de.themorpheus.edu.taskservice.util.ControllerResult;
 import java.util.List;
+
+import de.themorpheus.edu.taskservice.util.Validation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -24,4 +26,11 @@ public class SubjectController {
 		return ControllerResult.of(this.subjectRepository.findAll());
 	}
 
+    public void deleteSubject(String displayName) {
+		SubjectModel subjectModel = this.subjectRepository.getSubjectByDisplayNameIgnoreCase(displayName);
+
+		if (Validation.validateNull(subjectModel)) return;
+
+		this.subjectRepository.deleteById(subjectModel.getSubjectId());
+    }
 }
