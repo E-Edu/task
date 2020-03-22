2c45356091bae33a15d5ce7313f73bedd5b5aaae
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-03-22 21:36:55 +0100

    Changed annotations

--- src/main/java/de/themorpheus/edu/taskservice/endpoint/SubjectEndpoint.java
+++ src/main/java/de/themorpheus/edu/taskservice/endpoint/SubjectEndpoint.java
@@ -6,13 +6,13 @@ import de.themorpheus.edu.taskservice.util.Error;
 import de.themorpheus.edu.taskservice.util.Validation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
 
 @RestController
 public class SubjectEndpoint {
@@ -20,7 +20,7 @@ public class SubjectEndpoint {
 	@Autowired private SubjectController subjectController;
 
 	@PostMapping(value = "/subject", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Object createSubject(@RequestBody CreateSubjectDTO dto) {
+	public Object createSubject(@RequestBody @Valid CreateSubjectDTO dto) {
 		String displayName = dto.getDisplayName();
 
 		if (Validation.validateNull(displayName)) return Error.INVALID_PARAM;
@@ -31,9 +31,7 @@ public class SubjectEndpoint {
 	}
 
 	@GetMapping("/subject/{displayName}")
-	public Object getSubject(@PathVariable String displayName) {
-		if (Validation.nullOrEmpty(displayName)) return Error.INVALID_PARAM;
-
+	public Object getSubject(@PathVariable @Valid String displayName) {
 		return this.subjectController.getSubjectByDisplayName(displayName).getHttpResponse();
 	}
 
@@ -42,13 +40,8 @@ public class SubjectEndpoint {
 		return this.subjectController.getAllSubjects().getHttpResponse();
 	}
 
-	@RequestMapping(
-		method = RequestMethod.DELETE,
-		path = "/subject/{displayName}"
-	)
-	public void deleteSubject(@PathVariable String displayName) {
-		if (!Validation.validateNotNullOrEmpty(displayName)) return;
-
+	@DeleteMapping(path = "/subject/{displayName}")
+	public void deleteSubject(@PathVariable @Valid String displayName) {
 		this.subjectController.deleteSubject(displayName);
 	}
 
