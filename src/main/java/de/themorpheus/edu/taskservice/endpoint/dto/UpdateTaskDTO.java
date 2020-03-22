fb2b370c25ddaafef42d28fb7dec9ce5ada028ae
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-03-22 18:58:52 +0100

    Removed unused imports

--- src/main/java/de/themorpheus/edu/taskservice/endpoint/dto/UpdateTaskDTO.java
+++ src/main/java/de/themorpheus/edu/taskservice/endpoint/dto/UpdateTaskDTO.java
@@ -1,12 +1,8 @@
 package de.themorpheus.edu.taskservice.endpoint.dto;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 @Data
 @AllArgsConstructor
