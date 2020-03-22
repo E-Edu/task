37179d99c7f9a3ab21e4ebd488d97571e79f0136
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-03-22 16:18:22 +0100

    Created edit task endpoint

--- src/main/java/de/themorpheus/edu/taskservice/endpoint/TaskEndpoint.java
+++ src/main/java/de/themorpheus/edu/taskservice/endpoint/TaskEndpoint.java
@@ -2,17 +2,15 @@ package de.themorpheus.edu.taskservice.endpoint;
 
 import de.themorpheus.edu.taskservice.controller.TaskController;
 import de.themorpheus.edu.taskservice.endpoint.dto.CreateTaskDTO;
+import de.themorpheus.edu.taskservice.endpoint.dto.UpdateTaskDTO;
 import de.themorpheus.edu.taskservice.util.Validation;
 import java.util.UUID;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
 
 @RestController
 public class TaskEndpoint {
@@ -53,6 +51,34 @@ public class TaskEndpoint {
 		if (Validation.lowerZero(taskId)) return Error.INVALID_PARAM;
 
 		return this.taskController.verifyTask(taskId);
+	}
+
+	@RequestMapping(
+		method = RequestMethod.PUT,
+		path = "/task/{taskId}",
+		consumes = MediaType.APPLICATION_JSON_VALUE
+	)
+	@Transactional
+	public Object updateTask(@PathVariable int taskId, @RequestBody UpdateTaskDTO updateTaskDTO) {
+		if (Validation.nullOrEmpty(
+				updateTaskDTO.getTask(),
+				updateTaskDTO.getTaskTypeDisplayName(),
+				updateTaskDTO.getSubjectDisplayName(),
+				updateTaskDTO.getModuleDisplayName(),
+				updateTaskDTO.getLectureDisplayName(),
+				updateTaskDTO.getDifficultyDisplayName()
+			)
+			|| Validation.lowerZero(updateTaskDTO.getNecessaryPoints())
+		) return Error.INVALID_PARAM;
+
+		return this.taskController.updateTask(
+			taskId,
+			updateTaskDTO.getTask(),
+			updateTaskDTO.getNecessaryPoints(),
+			updateTaskDTO.getTaskTypeDisplayName(),
+			updateTaskDTO.getLectureDisplayName(),
+			updateTaskDTO.getDifficultyDisplayName()
+		);
 	}
 
 }
