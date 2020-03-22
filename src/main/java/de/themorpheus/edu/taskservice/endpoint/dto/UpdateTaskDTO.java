ec22b076a3d8dd2ba0db56732aa960b7dce6ea07
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-03-22 18:54:31 +0100

    Added annotations

--- src/main/java/de/themorpheus/edu/taskservice/endpoint/dto/UpdateTaskDTO.java
+++ src/main/java/de/themorpheus/edu/taskservice/endpoint/dto/UpdateTaskDTO.java
@@ -1,14 +1,21 @@
 package de.themorpheus.edu.taskservice.endpoint.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateTaskDTO {
 
+	@NotNull
+	@NotEmpty
+	@NotBlank
 	private String task;
 	private int necessaryPoints;
 
