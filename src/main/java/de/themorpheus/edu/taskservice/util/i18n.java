ec6016a983d48113e310dfaa5faea4413f4fc4da
Steve Hb <stve.hb@gmail.com>
2020-03-22 03:13:06 +0100

    Implemented basic i18n, validation and error

---
+++ src/main/java/de/themorpheus/edu/taskservice/util/i18n.java
@@ -0,0 +1,11 @@
+package de.themorpheus.edu.taskservice.util;
+
+public class i18n {
+
+	public static final String MISSING_PARAM = "MISSING_PARAM";
+	public static final String INVALID_PARAM = "INVALID_PARAM";
+	public static final String UNAUTHORIZED = "UNAUTHORIZED";
+	public static final String FORBIDDEN = "FORBIDDEN";
+	public static final String SERVER_ERROR = "SERVER_ERROR";
+
+}
