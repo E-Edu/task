64bdf78701acf0380852cecf34a1c986310cf795
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-03-22 18:49:34 +0100

    Replaced RequestMapping with PutMapping

--- src/main/java/de/themorpheus/edu/taskservice/endpoint/TaskEndpoint.java
+++ src/main/java/de/themorpheus/edu/taskservice/endpoint/TaskEndpoint.java
@@ -9,14 +9,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TaskEndpoint {
@@ -51,11 +44,7 @@ public class TaskEndpoint {
 		return this.taskController.verifyTask(taskId);
 	}
 
-	@RequestMapping(
-		method = RequestMethod.PUT,
-		path = "/task/{taskId}",
-		consumes = MediaType.APPLICATION_JSON_VALUE
-	)
+	@PutMapping(path = "/task/{taskId}"	)
 	public Object updateTask(@PathVariable @Min(0) int taskId, @RequestBody @Valid UpdateTaskDTO updateTaskDTO) {
 		return this.taskController.updateTask(
 			taskId,
