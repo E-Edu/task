a63ce80a36a82d6ab58a10df1b9bdaabbb947019
Steve Hb <stve.hb@gmail.com>
2020-03-22 03:13:51 +0100

    Added AllArgsConstructor to every model

--- src/main/java/de/themorpheus/edu/taskservice/database/model/DifficultyModel.java
+++ src/main/java/de/themorpheus/edu/taskservice/database/model/DifficultyModel.java
@@ -5,11 +5,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table
 public class DifficultyModel {
