7f78912420aaa4c9e86e1664db14cd79a702197c
Steve Hb <stve.hb@gmail.com>
2020-03-31 23:01:44 +0200

    Implemented log4j2 logging and sentry

--- src/main/java/de/themorpheus/edu/taskservice/TaskServiceApplication.java
+++ src/main/java/de/themorpheus/edu/taskservice/TaskServiceApplication.java
@@ -1,6 +1,8 @@
 package de.themorpheus.edu.taskservice;
 
 import de.themorpheus.edu.taskservice.cli.CLIManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,6 +11,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(scanBasePackages = "de.themorpheus.edu.taskservice")
 public class TaskServiceApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(TaskServiceApplication.class.getSimpleName());
+
 	public static final boolean PRODUCTIVE = Boolean.getBoolean(System.getenv("PRODUCTIVE"));
 
 	public static void main(String[] args) {
@@ -16,6 +20,11 @@ public class TaskServiceApplication {
 		SpringApplication.run(TaskServiceApplication.class, args);
 
 		CLIManager.initCli(args);
+
+		System.out.println("LEL");
+		LOGGER.info("Initialized Task-MicroService");
+		LOGGER.warn("Initialized Task-MicroService");
+		LOGGER.error("Initialized Task-MicroService");
 
 	}
 
