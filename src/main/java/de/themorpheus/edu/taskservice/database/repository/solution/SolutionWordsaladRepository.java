2eb2dc21e1301e5bc82be3e57e20a16cfec0290a
PAException <huerkamp.paul@gmail.com>
2020-04-14 19:42:22 +0200

    Fixed MultipleChoiceSolution and WordsaladSolution because they still handled with taskIds (#196)

--- src/main/java/de/themorpheus/edu/taskservice/database/repository/solution/SolutionWordsaladRepository.java
+++ src/main/java/de/themorpheus/edu/taskservice/database/repository/solution/SolutionWordsaladRepository.java
@@ -1,10 +1,18 @@
 package de.themorpheus.edu.taskservice.database.repository.solution;
 
+import de.themorpheus.edu.taskservice.database.model.solution.SolutionModel;
 import de.themorpheus.edu.taskservice.database.model.solution.WordsaladSolutionModel;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SolutionWordsaladRepository extends JpaRepository<WordsaladSolutionModel, Integer> {
+
+	boolean existsBySolutionId(SolutionModel solutionId);
+
+	Optional<WordsaladSolutionModel> findBySolutionId(SolutionModel solutionId);
+
+	void deleteBySolutionId(SolutionModel solutionId);
 
 }
