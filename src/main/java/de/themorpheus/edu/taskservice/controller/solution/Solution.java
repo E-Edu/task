b810a32010212ec3ec9dc4ee6dcb76ffaa21bb1f
PAException <huerkamp.paul@gmail.com>
2020-04-17 22:50:04 +0200

    Fixed Error on task delete (#182)

--- src/main/java/de/themorpheus/edu/taskservice/controller/solution/Solution.java
+++ src/main/java/de/themorpheus/edu/taskservice/controller/solution/Solution.java
@@ -1,11 +1,23 @@
 package de.themorpheus.edu.taskservice.controller.solution;
 
 import de.themorpheus.edu.taskservice.database.model.solution.SolutionModel;
+import javax.annotation.PostConstruct;
 
 interface Solution {
 
 	void deleteAll(int taskId);
 
 	void deleteSolutionIdIfDatabaseIsEmpty(SolutionModel solutionId);
+
+	/**
+	 * Registers the <i>GenericSolutionController</i> in <i>SolutionController</i>.
+	 * <b>Please do not override this method</b>
+	 *
+	 * @see SolutionController
+	 */
+	@PostConstruct
+	default void registerGenericSolutionController() {
+		SolutionController.registerGenericSolutionController(this);
+	}
 
 }
